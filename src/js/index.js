79561142ff100c98cb8757d90ed3d7ab7cd04cb0
Jose D. Vasquez <jodavaga@gmail.com>
2020-02-04 09:46:51 -0500

    Hover, active styles added. dropdown menu implemented

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,13 @@
 // Dropdown actions
 var themeButton = document.getElementById('theme-selection');
+var themeButtonPrimary = document.getElementById('selection-button');
 var lightThemeButton = document.getElementById('light-btn');
 var darkThemeButton = document.getElementById('dark-btn');
 
 themeButton.addEventListener('click', (e) => {
     document.getElementsByClassName('dropdown-menu')[0].classList.toggle('hidden');
+    themeButtonPrimary.classList.toggle('active');
+    themeButton.classList.toggle('active');
 });
 
 // Add dark theme
@@ -12,6 +15,12 @@ darkThemeButton.addEventListener('click', (e) => {
     document.getElementsByTagName('body')[0].classList.remove('light-theme');
     document.getElementsByTagName('body')[0].classList.add('dark-theme');
     document.getElementsByClassName('dropdown-menu')[0].classList.toggle('hidden');
+    themeButton.classList.toggle('active');
+    themeButtonPrimary.classList.toggle('active');
+
+    // add active class to btn-info
+    lightThemeButton.classList.remove('active');
+    e.target.parentElement.classList.toggle('active');
 
 });
 
@@ -20,5 +29,9 @@ lightThemeButton.addEventListener('click', (e) => {
     document.getElementsByTagName('body')[0].classList.remove('dark-theme');
     document.getElementsByTagName('body')[0].classList.add('light-theme');
     document.getElementsByClassName('dropdown-menu')[0].classList.toggle('hidden');
-
+    themeButton.classList.toggle('active');
+    themeButtonPrimary.classList.toggle('active');
+    
+    darkThemeButton.classList.remove('active');
+    e.target.parentElement.classList.toggle('active');
 });
